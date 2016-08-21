79262e283985158bab760fe0d8481a53e171f84b
CocoaMel <nicole.ang94@gmail.com>
2016-08-20 22:59:52 -0700

    Added some final packs for now, completing all packs currently in boutique. Renamed a var in helper.

--- pwi/public/js/helper.js
+++ pwi/public/js/helper.js
@@ -5,22 +5,23 @@ $(function () {
     $("#process").on("click", function (event) {
         var textVal = $("#packSection").val();
         $("#packSection").val(" ");
-        var res = textVal.split("\n");
+        var textValAsArray = textVal.split("\n");
         var colonIndex;
         var numSpaces = 0;
         var spaceArray = [];
         var spaceString = "";
-        for (var i = 0 ; i < res.length; i++) {
-            console.log(res[i]);
-            colonIndex = res[i].indexOf("\" : \"");
+        for (var i = 0 ; i < textValAsArray.length; i++) {
+            console.log(textValAsArray[i]);
+            colonIndex = textValAsArray[i].indexOf("\" : \"");
             if (colonIndex < 32) {
                 numSpaces = 32 - colonIndex;
                 spaceArray = new Array(numSpaces - 1).fill(" ");
                 spaceString = spaceArray.join("");
-                res[i] = res[i].substring(0, colonIndex + 2) + spaceString + res[i].substring(colonIndex + 1, res[i].length);
+                textValAsArray[i] = textValAsArray[i].substring(0, colonIndex + 2) + 
+                    spaceString + textValAsArray[i].substring(colonIndex + 1, textValAsArray[i].length);
             }
             $("#packSection").val(function () {
-                return $(this).val() + "\n" + res[i];
+                return $(this).val() + "\n" + textValAsArray[i];
             });
         };
         console.log("done");
@@ -29,8 +30,8 @@ $(function () {
     $("#reverse").on("click", function (event) {
         var textVal = $("#packSection").val();
         $("#packSection").val(" ");
-        var res = textVal.split("\n");
-        var reverse = res.reverse();
+        var textValAsArray = textVal.split("\n");
+        var reverse = textValAsArray.reverse();
         for (var i = 0; i < reverse.length; i++) {
             $("#packSection").val(function () {
                 return $(this).val() + "\n" + reverse[i];
